b6a89d4839e1da51819b885485193cd53be9fba8
wanchuanheng <240650133@qq.com>
2016-06-14 11:28:35 +0800

    add example
    add example

--- thread.cpp
+++ thread.cpp
@@ -23,6 +23,12 @@ void f2(int& n)
     }
 }
 
+void thread_task(int n) {
+    std::this_thread::sleep_for(std::chrono::seconds(n));
+    std::cout << "hello thread "
+        << std::this_thread::get_id()
+        << " paused " << n << " seconds" << std::endl;
+}
 
 
 int main(int argc, const char *argv[])
@@ -37,6 +43,18 @@ int main(int argc, const char *argv[])
     t4.join();
     t.join();
     std::cout << "Final value of n is " << n << '\n';
+    
+    std::thread threads[5];
+    std::cout << "Spawning 5 threads...\n";
+    for (int i = 0; i < 5; i++) {
+        threads[i] = std::thread(thread_task, i + 1);
+    }
+    std::cout << "Done spawning threads! Now wait for them to join\n";
+    for (auto& t: threads) {
+        t.join();
+    }
+    std::cout << "All threads joined.\n";
+    
     
     
     
